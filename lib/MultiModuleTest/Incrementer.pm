01d6c509a2f2afa7d96098155b727c661b0551c3
Dana M. Diederich <diederich@gmail.com>
2013-08-15 04:54:18 +0000

    Import of DIEDERICH/App-MultiModule-1.132270 from CPAN.
    gitpan-cpan-distribution: App-MultiModule
    gitpan-cpan-version:      1.132270
    gitpan-cpan-path:         DIEDERICH/App-MultiModule-1.132270.tar.gz
    gitpan-cpan-author:       DIEDERICH
    gitpan-cpan-maturity:     released

---
+++ lib/MultiModuleTest/Incrementer.pm
@@ -0,0 +1,23 @@
+package MultiModuleTest::Incrementer;
+{
+  $MultiModuleTest::Incrementer::VERSION = '1.132270';
+}
+
+use strict;use warnings;
+use Data::Dumper;
+
+use parent 'App::MultiModule::Task';
+
+
+=head2 message
+
+=cut
+sub message {
+    my $self = shift;
+    my $message = shift;
+    $self->debug('Incrementer message: ' . Data::Dumper::Dumper $message) if $self->{debug};
+    $message->{i} = $message->{i} + 1;
+    $self->emit($message);
+}
+
+1;
